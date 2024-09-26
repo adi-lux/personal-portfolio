abe6faae0f526b4333306537bc45a1631306b4f3
Adithya Anandsaikrishnan <anandsaikrishnan.adithya@gmail.com>
2024-09-26 00:04:35 -0700

    feat: setup application with initial codebase
    Signed-off-by: Adithya Anandsaikrishnan <anandsaikrishnan.adithya@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  plugins: [daisyui],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: ['retro', 'synthwave'],
+  },
+  darkMode: ['selector', '[data-theme="synthwave"]'],
 };
 export default config;
